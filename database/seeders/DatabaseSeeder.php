ce818cf42b141759f6f4cb062db5794113bbd1af
mkthrkw <mkt1536@gmail.com>
2023-12-30 18:27:53 +0900

    Seeder/routeファイル追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            BcRouteSeeder::class,
+            AccountSeeder::class,
+            DepartmentSeeder::class,
+            DesktopPcSeeder::class,
+            LaptopPcSeeder::class,
+            MailingListSeeder::class,
+            MobilePhoneSeeder::class,
+            AccountBcRouteSeeder::class,
+            AccountDepartmentSeeder::class,
+            AccountMailingListSeeder::class,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,38 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// Top
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.top');
+})->middleware('auth')->name('top');
+
+// Pages
+Route::middleware('auth')->group(function (){
+
+    // Additonal pages
+    Route::get('department/chart','App\Http\Controllers\DepartmentController@chart')->name('department.chart');
+    Route::group(['prefix' => 'develop', 'as' => 'develop.'],function (){
+        Route::get('sql','App\Http\Controllers\DevelopController@sql')->name('sql.get');
+        Route::post('sql','App\Http\Controllers\DevelopController@sqlExecute')->name('sql.post');
+    });
+
+
+    // Basic resouces
+    Route::resource('account', 'App\Http\Controllers\AccountController');
+
+
+    // Device resouces
+    Route::resource('desktop_pc', 'App\Http\Controllers\DesktopPcController');
+    Route::resource('laptop_pc', 'App\Http\Controllers\LaptopPcController');
+    Route::resource('mobile_phone', 'App\Http\Controllers\MobilePhoneController');
+
+
+    // Manage resouces
+    Route::resource('mailing_list', 'App\Http\Controllers\MailingListController');
+    Route::resource('bc_route', 'App\Http\Controllers\BcRouteController');
+    Route::resource('department', 'App\Http\Controllers\DepartmentController');
+
 });
+
+
+require __DIR__.'/auth.php';
